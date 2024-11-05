2f77907cc29b653b60c6338fb13f55747941072a
ccisse2 <cheikh.cisse2024@campus-eni.fr>
2024-11-05 10:40:11 +0100

    test db connexion

--- db.js
+++ db.js
@@ -1,39 +1,28 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const  db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
+    database: process.env.DB_NAME,
+    waitForConnections: true,      // Attend que les connexions soient disponibles
+    connectionLimit: 10,           // Limite du nombre de connexions simultanées (ajustez selon vos besoins)
+    queueLimit: 0                  // Pas de limite pour la file d'attente
+});
 
-db.connect(err => {
+// Vérifier la connexion initiale au démarrage
+db.getConnection((err, connection) => {
     if (err) {
-    }else{
+        console.error('Erreur de connexion à la base de données:', err);
+    } else {
+        console.log('Connexion à la base de données réussie');
+        connection.release(); // Relâche la connexion après le test
     }
-})
+});
 
 module.exports = db;
 
 
 
-db.connect(err => {
-    if (err) {
-        console.error('Erreur de connexion à la base de données :', err);
-    } else {
-        console.log('Connexion à la base de données réussie');
 
-        // Exécuter une simple requête pour vérifier la connexion
-        db.query('SELECT 1', (err, results) => {
-            if (err) {
-                console.error('Erreur lors de l\'exécution de la requête :', err);
-            } else {
-                console.log('Résultat de la requête :', results);
-            }
-
-            // Fermer la connexion après le test
-            db.end();
-        });
-    }
-});
